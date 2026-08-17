41e52ba6051ae6d6bd8d1ebacb034c27ebb1bcdb
arifulislamk <ariful18374@gmail.com>
2026-08-17 21:47:12 +0600

    navbar design

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,87 +1,162 @@
 import React from "react";
-import logo from "../assets/web logo khan market.png"
+import logo from "../assets/web logo khan market.png";
+
 const Navbar = () => {
   return (
-    <div className="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
-      <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
-      <label
-        htmlFor="navbar-1-toggle"
-        className="fixed inset-0 hidden max-lg:peer-checked:block"
-      ></label>
-      <div className="collapse-title navbar">
-        <div className="navbar-start">
-          <label htmlFor="navbar-1-toggle" className="btn btn-ghost lg:hidden">
+    <div className="w-full bg-base-200 rounded-md shadow-sm">
+      
+      <div className="navbar w-full px-2 sm:px-3 lg:px-6 gap-1 sm:gap-2">
+
+        {/* ================= LOGO ================= */}
+        <div className="shrink-0">
+          <button className="btn btn-ghost px-0 sm:px-2">
+            <img
+              src={logo}
+              alt="Khan Market"
+              className="w-16 sm:w-20 lg:w-28 rounded-xl"
+            />
+          </button>
+        </div>
+
+
+        {/* ================= SEARCH ================= */}
+        <div className="flex-1 min-w-0">
+          <div className="relative w-full">
+
+            {/* Search Input */}
+            <input
+              type="search"
+              placeholder="Search products..."
+              className="
+                w-full
+                h-9
+                sm:h-10
+                lg:h-12
+
+                rounded-lg
+
+                border-2
+                border-amber-400
+
+                bg-white
+
+                text-gray-900
+                placeholder-gray-500
+
+                text-center
+                text-xs
+                sm:text-sm
+                lg:text-base
+
+                font-medium
+
+                pl-3
+                pr-10
+
+                outline-none
+
+                focus:border-amber-500
+                focus:ring-2
+                focus:ring-amber-200
+
+                transition-all
+              "
+            />
+
+            {/* Search Button - RIGHT SIDE */}
+            <button
+              type="button"
+              title="Search"
+              className="
+                absolute
+                right-2
+                top-1/2
+                -translate-y-1/2
+                flex
+                items-center
+                justify-center
+                w-7
+                h-7
+                sm:w-8
+                sm:h-8
+                rounded-md
+                text-gray-600
+                hover:bg-amber-100
+                hover:text-amber-600
+                transition
+              "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="w-4 h-4 sm:w-5 sm:h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.197 5.197a7.5 7.5 0 0 0 10.606 10.606Z"
+                />
+              </svg>
+            </button>
+
+          </div>
+        </div>
+
+
+        {/* ================= CART ================= */}
+        <div className="shrink-0">
+          <button
+            className="
+              btn
+              btn-ghost
+              btn-sm
+              p-1
+              sm:p-2
+            "
+            title="Cart"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
+              strokeWidth={1.8}
               stroke="currentColor"
+              className="w-5 h-5 sm:w-6 sm:h-6"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25h9.75m-9.75 0a2.25 2.25 0 0 0-2.25 2.25v.75h14.25v-.75a2.25 2.25 0 0 0-2.25-2.25m-9.75 0L4.5 5.25h14.625a1.125 1.125 0 0 1 1.08 1.44l-1.5 5.25a1.125 1.125 0 0 1-1.08.81H7.5Z"
               />
             </svg>
-          </label>
-          <button className="btn btn-ghost text-xl">
-            <img className=" w-28 rounded-xl " src={logo} alt="" srcset="" />
           </button>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <button>Item 1</button>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <button>Submenu 1</button>
-                  </li>
-                  <li>
-                    <button>Submenu 2</button>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <button>Item 3</button>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input w-64 lg:w-auto"
-          />
-        </div>
-      </div>
 
-      <div className="collapse-content lg:hidden z-1">
-        <ul className="menu">
-          <li>
-            <button>Item 1</button>
-          </li>
-          <li>
-            <button>Parent</button>
-            <ul>
-              <li>
-                <button>Submenu 1</button>
-              </li>
-              <li>
-                <button>Submenu 2</button>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <button>Item 3</button>
-          </li>
-        </ul>
+
+        {/* ================= PROFILE ================= */}
+        <div className="shrink-0">
+          <button
+            className="btn btn-ghost btn-sm p-1"
+            title="Profile"
+          >
+            <img
+              src="https://i.pravatar.cc/100?img=12"
+              alt="Profile"
+              className="
+                w-6
+                h-6
+                sm:w-8
+                sm:h-8
+                rounded-full
+                object-cover
+              "
+            />
+          </button>
+        </div>
+
       </div>
     </div>
   );
